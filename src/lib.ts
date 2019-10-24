f43224e3999d93f3b04f205d4fb8512b25cf4fbd
Jianwu Chen <jianwu@gmail.com>
2019-10-24 09:26:23 -0700

    change license

--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,8 @@
 import Vue from 'vue';
 import MSplit from './components/MSplit.vue';
-import OnResize, { OnResizeDirective } from './components/OnResize';
 
 export default {
   install(vue: typeof Vue) {
-    // debugger;
-    // console.log('install OnResize');
-    // vue.use(OnResize);
-    // vue.directive('on-resize', OnResizeDirective.instance);
-    console.log('install msplit');
     vue.component('msplit', MSplit);
   },
 };
